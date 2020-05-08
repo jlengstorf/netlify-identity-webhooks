321fda7743d455431f1685030838bff899b030df
Jason Lengstorf <jason@lengstorf.com>
2020-05-08 09:05:06 -0700

    feat: role test

--- functions/role-test.js
+++ functions/role-test.js
@@ -2,10 +2,10 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
   console.log(context);
-  const { userID } = JSON.parse(event.body);
-  const { identity, user } = context.clientContext;
+  const { userID, role = 'default' } = JSON.parse(event.body);
+  const { identity } = context.clientContext;
 
-  console.log({ userID });
+  console.log({ userID, role });
 
   const response = await fetch(`${identity.url}/admin/users/${userID}`, {
     method: 'PUT',
@@ -14,7 +14,7 @@ exports.handler = async (event, context) => {
     },
     body: JSON.stringify({
       app_metadata: {
-        roles: ['custom'],
+        roles: ['custom', role],
       },
     }),
   })
