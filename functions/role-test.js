bb4d0c9a508ee3d10a9cc2f6b9d2c791bda88c2e
Jason Lengstorf <jason@lengstorf.com>
2020-05-07 23:01:43 -0700

    feat: roles

--- functions/role-test.js
+++ functions/role-test.js
@@ -2,11 +2,12 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
   console.log(context);
+  const { identity, user } = context.clientContext;
 
-  await fetch(`${context.identity.url}/admin/users/${context.user.id}`, {
+  const response = await fetch(`${identity.url}/admin/users/${user.sub}`, {
     method: 'PUT',
     headers: {
-      Authorization: `Bearer ${context.identity.token}`,
+      Authorization: `Bearer ${identity.token}`,
     },
     body: JSON.stringify({
       app_metadata: {
@@ -16,6 +17,8 @@ exports.handler = async (event, context) => {
   })
     .then((res) => res.json())
     .catch((err) => console.error(err));
+
+  console.log(response);
 
   return {
     statusCode: 200,
